b639166aeb0957ac2ffb52d76d5a00869faaee8f
silva4dev <web4dev.lucas@outlook.com>
2024-08-09 18:40:11 -0300

    test: 🧪 add delete product

--- src/test1.spec.ts
+++ src/test1.spec.ts
@@ -1,48 +1,72 @@
 import { DataSource } from "typeorm";
 import { Product } from "./Product";
 import { ProductService } from "./ProductService";
-import { MySqlContainer } from "@testcontainers/mysql";
+import { MySqlContainer, StartedMySqlContainer } from "@testcontainers/mysql";
 
-// // image não existir
-//    - baixar a imagem
-//    - inicializar o container
-// // imagem já existir
-//    - inicializar o container
+describe("ProductService Tests", () => {
+  let mysqlContainer: StartedMySqlContainer;
+  let dataSource: DataSource;
 
-test("create product", async () => {
-  const mysqlContainer = await new MySqlContainer("mysql:8.0.30-debian")
-    .withCommand(["--default-authentication-plugin=mysql_native_password"])
-    .start();
+  beforeEach(async () => {
+    mysqlContainer = await new MySqlContainer("mysql:8.0.30-debian")
+      .withCommand(["--default-authentication-plugin=mysql_native_password"])
+      .start();
 
-  const dataSource = new DataSource({
-    type: "mysql",
-    host: mysqlContainer.getHost(),
-    port: mysqlContainer.getMappedPort(3306),
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    entities: [Product],
-    logging: true
+    dataSource = new DataSource({
+      type: "mysql",
+      host: mysqlContainer.getHost(),
+      port: mysqlContainer.getMappedPort(3306),
+      username: "test",
+      password: "test",
+      database: "test",
+      synchronize: true,
+      entities: [Product],
+      logging: true
+    });
+
+    await dataSource.initialize();
+  }, 200000);
+
+  afterEach(async () => {
+    await dataSource?.destroy();
+    await mysqlContainer?.stop();
   });
 
-  await dataSource.initialize();
+  test("create product", async () => {
+    const productService = new ProductService(
+      dataSource.getRepository(Product)
+    );
 
-  const productService = new ProductService(dataSource.getRepository(Product));
+    const product = await productService.createProduct({
+      name: "product",
+      price: 100
+    });
 
-  const product = await productService.createProduct({
-    name: "product",
-    price: 100
+    expect(product).toEqual({
+      id: expect.any(Number),
+      name: "product",
+      price: 100
+    });
   });
 
-  expect(product).toEqual({
-    id: expect.any(Number),
-    name: "product",
-    price: 100
+  test("delete product", async () => {
+    const productService = new ProductService(
+      dataSource.getRepository(Product)
+    );
+
+    const product = await productService.createProduct({
+      name: "product",
+      price: 100
+    });
+
+    await productService.deleteProduct(product.id);
+
+    const deletedProduct = await dataSource.getRepository(Product).findOne({
+      where: {
+        id: product.id
+      }
+    });
+
+    expect(deletedProduct).toBeNull();
   });
-
-  await dataSource.destroy();
-
-  await mysqlContainer.stop();
-}, 200000);
-
+});
