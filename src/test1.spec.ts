50250482218ef9a0a6c27adedd2778c1601a3fa6
silva4dev <web4dev.lucas@outlook.com>
2024-08-11 19:15:29 -0300

    test: :sparkles: add create new database to each test

--- src/test1.spec.ts
+++ src/test1.spec.ts
@@ -1,4 +1,4 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { Product } from "./Product";
 import { ProductService } from "./ProductService";
 import { MySqlContainer, StartedMySqlContainer } from "@testcontainers/mysql";
@@ -7,28 +7,55 @@ describe("ProductService Tests", () => {
   let mysqlContainer: StartedMySqlContainer;
   let dataSource: DataSource;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     mysqlContainer = await new MySqlContainer("mysql:8.0.30-debian")
       .withCommand(["--default-authentication-plugin=mysql_native_password"])
+      .withRootPassword("root")
       .start();
+  }, 200000);
+
+  beforeEach(async () => {
+    const connOptions: DataSourceOptions = {
+      type: "mysql",
+      host: mysqlContainer.getHost(),
+      port: mysqlContainer.getMappedPort(3306),
+      logging: true
+    };
+
+    const adminDataSource = new DataSource({
+      ...connOptions,
+      username: "root",
+      password: "root",
+      database: "test"
+    });
+
+    await adminDataSource.initialize();
+
+    const databaseName = "test_" + Math.random().toString(36).substring(7);
+
+    await adminDataSource.query(`CREATE DATABASE ${databaseName};`);
+    await adminDataSource.destroy();
 
     dataSource = new DataSource({
       type: "mysql",
       host: mysqlContainer.getHost(),
       port: mysqlContainer.getMappedPort(3306),
-      username: "test",
-      password: "test",
-      database: "test",
+      username: "root",
+      password: "root",
+      database: databaseName,
       synchronize: true,
       entities: [Product],
       logging: true
     });
 
     await dataSource.initialize();
-  }, 200000);
+  });
 
   afterEach(async () => {
     await dataSource?.destroy();
+  });
+
+  afterAll(async () => {
     await mysqlContainer?.stop();
   });
 
