aa8424fa62998a421f058e564533411b4e792660
silva4dev <web4dev.lucas@outlook.com>
2024-08-11 16:58:03 -0300

    test: :sparkles: add a new product

--- src/test1.spec.ts
+++ src/test1.spec.ts
@@ -3,15 +3,24 @@ import { Product } from "./Product";
 import { ProductService } from "./ProductService";
 import { MySqlContainer } from "@testcontainers/mysql";
 
-test("create product", async() => {
-  const mysqlContainer = await new MySqlContainer().start();
+// // image não existir
+//    - baixar a imagem
+//    - inicializar o container
+// // imagem já existir
+//    - inicializar o container
+
+test("create product", async () => {
+  const mysqlContainer = await new MySqlContainer("mysql:8.0.30-debian")
+    .withCommand(["--default-authentication-plugin=mysql_native_password"])
+    .start();
 
   const dataSource = new DataSource({
     type: "mysql",
-    host: "",
-    port: 0,
-    username: "",
-    password: "",
+    host: mysqlContainer.getHost(),
+    port: mysqlContainer.getMappedPort(3306),
+    username: "test",
+    password: "test",
+    database: "test",
     synchronize: true,
     entities: [Product],
     logging: true
@@ -31,4 +40,4 @@ test("create product", async() => {
     name: "product",
     price: 100
   });
-});
+}, 200000);
